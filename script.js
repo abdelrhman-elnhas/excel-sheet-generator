1e5db1f6ba0a6a5d920c999b5dc6bfa7ec321a66
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2023-06-23 20:35:23 +0300

    alerts added

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+  rows = document.getElementsByClassName("rows")[0],
+  columns = document.getElementsByClassName("columns")[0];
+tableExists = false;
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  let rowsNumber = parseInt(rows.value),
+    columnsNumber = parseInt(columns.value);
+  table.innerHTML = "";
+  for (let i = 0; i < rowsNumber; i++) {
+    var tableRow = "";
+    for (let j = 0; j < columnsNumber; j++) {
+      tableRow += `<td contenteditable></td>`;
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
-    }
-}
+    table.innerHTML += tableRow;
+  }
+  if (rowsNumber > 0 && columnsNumber > 0) {
+    tableExists = true;
+    Swal.fire("", "Table generated while the fields are empty.", "info");
+  }
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+  if (!tableExists) {
+    Swal.fire("", "There is no generated table to be exported.", "error");
+
+    return;
+  }
+  var elt = table;
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl
+    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+    : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
